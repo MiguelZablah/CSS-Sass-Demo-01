00a0a30a13b9fcc126299345cce471b67fb5bd93
Zablah <miguel.ramirez@diginteligencia.com>
2018-03-21 12:28:50 -0600

    seting up wacher with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,32 @@
 const gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	cleanCSS = require('gulp-clean-css'),
-	stripCssComments = require('gulp-strip-css-comments');
+	stripCssComments = require('gulp-strip-css-comments'),
+	browserSync = require('browser-sync').create();
 
 // Route Variables
 var sassSrc = 'sass/**/*.scss';
-var sassDist = 'css/';
+var htmlDir = 'app/*.html';
+var sassDir = 'app/css/';
 
 // Task Sass compiler
 gulp.task('sass', function () {
 	return gulp.src(sassSrc)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(stripCssComments())
+		// .pipe(stripCssComments())
 		// .pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(gulp.dest(sassDist));
+		.pipe(gulp.dest(sassDir))
+		.pipe(browserSync.stream());
 });
 
-// Task Sass wacher for changer
-gulp.task('sass:watch', function () {
+// Start server with sass wacher and html watcher
+gulp.task('serve', function () {
+	browserSync.init({
+		server: "./app"
+	});
+
 	gulp.watch(sassSrc, ['sass']);
+	gulp.watch(htmlDir).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['sass']);
