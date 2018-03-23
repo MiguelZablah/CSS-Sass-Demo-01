4a173bfe3fd1f1f20feae840fd00996bf74c403e
Zablah <miguel.ramirez@diginteligencia.com>
2018-03-23 16:16:22 -0600

    Seccion tour complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 // Requiered
 const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    cleanCSS = require('gulp-clean-css'),
-    stripCssComments = require('gulp-strip-css-comments'),
-    browserSync = require('browser-sync').create();
+	sass = require('gulp-sass'),
+	cleanCSS = require('gulp-clean-css'),
+	stripCssComments = require('gulp-strip-css-comments'),
+	browserSync = require('browser-sync').create();
 
 // Route Variables
 var sassSrc = 'sass/**/*.scss';
@@ -12,22 +12,22 @@ var sassDir = 'app/css/';
 
 // Task Sass compiler
 gulp.task('sass', function () {
-    return gulp.src(sassSrc)
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    // .pipe(stripCssComments())
-    // .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest(sassDir))
-        .pipe(browserSync.stream());
+	return gulp.src(sassSrc)
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	// .pipe(stripCssComments())
+	// .pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest(sassDir))
+		.pipe(browserSync.stream());
 });
 
 // Start server with sass wacher and html watcher
 gulp.task('serve', function () {
-    browserSync.init({
-        server: "./app"
-    });
+	browserSync.init({
+		server: "./app"
+	});
 
-    gulp.watch(sassSrc, ['sass']);
-    gulp.watch(htmlDir).on('change', browserSync.reload);
+	gulp.watch(sassSrc, ['sass']);
+	gulp.watch(htmlDir).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['sass']);
